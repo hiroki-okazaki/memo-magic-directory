8b251e25fefc4bf195fe58f2d5bd8461682e61fb
Hiroki Okazaki <>
2021-10-29 00:19:18 +0900

    create react app

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node:15.14.0-alpine3.11
+
+RUN yarn install
---
+++ src/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// import 'admin-lte/plugins/fontawesome-free/css/all.min.css';
+// import 'admin-lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css';
+// import 'admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css';
+// import 'admin-lte/plugins/jqvmap/jqvmap.min.css';
+// import 'admin-lte/dist/css/adminlte.min.css';
+// import 'admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css';
+// import 'admin-lte/plugins/daterangepicker/daterangepicker.css';
+// import 'admin-lte/plugins/summernote/summernote-bs4.css';
+
+// import 'admin-lte/plugins/jquery/jquery.min.js';
+// import 'admin-lte/plugins/jquery-ui/jquery-ui.min.js';
+// import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+// import 'admin-lte/plugins/chart.js/Chart.min.js';
+// import 'admin-lte/plugins/sparklines/sparkline.js';
+
+// // import 'admin-lte/plugins/jqvmap/jquery.vmap.min.js';
+// // import 'admin-lte/plugins/jqvmap/maps/jquery.vmap.usa.js';
+
+// import 'admin-lte/plugins/jquery-knob/jquery.knob.min.js';
+// import 'admin-lte/plugins/moment/moment.min.js';
+// import 'admin-lte/plugins/daterangepicker/daterangepicker.js';
+
+// // import 'admin-lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js';
+
+// // import 'admin-lte/plugins/summernote/summernote-bs4.min.js';
+// import 'admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js';
+// import 'admin-lte/dist/js/adminlte.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
